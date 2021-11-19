2d078b5fb3f42a7df82ee7f546d33e09612f849f
vincemann <v.conrad98@web.de>
2021-11-19 15:53:04 +0100

    remove logging

--- show-last-dirs.py
+++ show-last-dirs.py
@@ -15,17 +15,17 @@ if len(sys.argv) > 1:
         ezlib.eprint("gui was: %s" % gui)
         ezlib.eprint("usage: python3 show-last-dirs.py gui|terminal [matchword [last-dirs-amount=num default 10 [ clip=[0|1] default 0]]]")
         exit(1)
-    ezlib.eprint("gui is: %s" % gui)
+    # ezlib.eprint("gui is: %s" % gui)
 
 
 
 if len(sys.argv) > 2:
     matchword = sys.argv[2]
-    ezlib.eprint("matchword is: %s" % matchword)
+    # ezlib.eprint("matchword is: %s" % matchword)
 
 if len(sys.argv) > 3:
     last_dirs_amount = int(sys.argv[3])
-    ezlib.eprint("last_dirs_amount is: %d" % last_dirs_amount)
+    # ezlib.eprint("last_dirs_amount is: %d" % last_dirs_amount)
 
 
 if len(sys.argv) > 4:
@@ -34,14 +34,14 @@ if len(sys.argv) > 4:
         ezlib.eprint("clip was %d" % clip)
         ezlib.eprint("usage: python3 show-last-dirs.py gui|terminal [matchword [last-dirs-amount=num default 10 [ clip=[0|1] default 0]]]")
         exit(1)
-    ezlib.eprint("clip is %d" % clip)
+    # ezlib.eprint("clip is %d" % clip)
 
 dir_history_file = os.getenv("DIR_HISTORY")
 if dir_history_file == None or dir_history_file == "":
     ezlib.eprint("cannot find dir dir history-file, set DIR_HISTORY env var")
     exit(1)
 
-ezlib.eprint("dir hist file: %s" % dir_history_file)
+# ezlib.eprint("dir hist file: %s" % dir_history_file)
 
 
 import signal
@@ -50,14 +50,14 @@ def sigint_handler(signal, frame):
 
 signal.signal(signal.SIGINT, sigint_handler)
 last_dirs = ezlib.find_recent_dirs(dir_history_file,last_dirs_amount,matchword)
-ezlib.eprint("last dirs: %s" % last_dirs)
+# ezlib.eprint("last dirs: %s" % last_dirs)
 
 if gui == "gui":
     selected_dir = ezlib.show_gui_selection(last_dirs)
 else:
     selected_dir = ezlib.show_terminal_selection(last_dirs)
 
-ezlib.eprint("selected_dir: %s" % selected_dir)
+# ezlib.eprint("selected_dir: %s" % selected_dir)
 
 if selected_dir is None:
     ezlib.eprint("nothing selected - closing")
@@ -66,6 +66,6 @@ if clip == 1:
     ezlib.eprint("putting to clipboard")
     ezlib.put_to_clipboard(selected_dir)
 else:
-    ezlib.eprint("returning dir")
+    # ezlib.eprint("returning dir")
     # put out as return value for calling bash scripts
     print(selected_dir)
